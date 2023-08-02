b861e13a900d335ed64adb990c6ea614903e19be
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-08-02 19:56:23 +0100

    working on infrastructures

---
+++ app/src/main/java/com/reza/newsapp/navigation/AppNavGarph.kt
@@ -0,0 +1,19 @@
+package com.reza.newsapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun AppNavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = ""
+    ) {
+
+    }
+}
